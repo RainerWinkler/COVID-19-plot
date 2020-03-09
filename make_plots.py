da3cfab19fa1f27d528c74251316508c9d4834a3
Rainer Winkler <rainer.winkler@poaceae.de>
2020-03-09 23:44:13 +0100

    Script to make plots

---
+++ make_plots.py
@@ -0,0 +1,4 @@
+import os
+
+os.system("python covid19plot.py -dark -deaths -out middle_europe france germany austria")
+os.system("python covid19plot.py -dark -out middle_europe_count france germany austria")
