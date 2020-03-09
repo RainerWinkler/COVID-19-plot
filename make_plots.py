5ca7137ec938d24e27e6c0f5ce6afe355bc17338
Rainer Winkler <rainer.winkler@poaceae.de>
2020-03-09 23:56:43 +0100

    Updated plots

--- make_plots.py
+++ make_plots.py
@@ -1,4 +1,4 @@
 import os
 
-os.system("python covid19plot.py -dark -deaths -out middle_europe france germany austria")
+os.system("python covid19plot.py -dark -deaths -out middle_europe_deaths france germany austria")
 os.system("python covid19plot.py -dark -out middle_europe_count france germany austria")
