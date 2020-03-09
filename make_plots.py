3fffa0d5aa4507efe303eda0db9f0c68839f54f4
Rainer Winkler <rainer.winkler@poaceae.de>
2020-03-10 00:02:13 +0100

    Add north europe to plots

--- make_plots.py
+++ make_plots.py
@@ -2,3 +2,6 @@ import os
 
 os.system("python covid19plot.py -dark -deaths -out middle_europe_deaths france germany austria")
 os.system("python covid19plot.py -dark -out middle_europe_count france germany austria")
+
+os.system("python covid19plot.py -dark -deaths -out north_europe_deaths sweden norway denmark")
+os.system("python covid19plot.py -dark -out north_europe_count sweden norway denmark")
