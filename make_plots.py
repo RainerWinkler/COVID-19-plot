28cd7b8aa7f04f1391b096021036a6ef45859500
Rainer Winkler <rainer.winkler@poaceae.de>
2020-03-10 19:36:22 +0100

    Add Italy

--- make_plots.py
+++ make_plots.py
@@ -1,7 +1,7 @@
 import os
 
-os.system("python covid19plot.py -dark -deaths -out middle_europe_deaths france germany austria")
-os.system("python covid19plot.py -dark -out middle_europe_count france germany austria")
+os.system("python covid19plot.py -dark -deaths -out middle_europe_deaths france germany austria italy")
+os.system("python covid19plot.py -dark -out middle_europe_count france germany austria italy")
 
 os.system("python covid19plot.py -dark -deaths -out north_europe_deaths sweden norway denmark")
 os.system("python covid19plot.py -dark -out north_europe_count sweden norway denmark")
