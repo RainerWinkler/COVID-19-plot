5d379a09c656c04dac6d4fdbe61b9d55091d540c
Rainer Winkler <rainer.winkler@poaceae.de>
2020-04-01 08:45:58 +0200

    Compare Germany and Asia

---
+++ Germany_versus_asia.py
@@ -0,0 +1,6 @@
+import os
+
+os.system('python covid19plot.py -perCapita -log -dark -deaths -out Germany_Asia_deaths -yFrom 1e-3 -yTo 1 -title "Asia deaths" CN KR JP TW TH VN KH SG MN DE')
+os.system('python covid19plot.py -perCapita -log -dark  -out Germany_Asia_Cases -yFrom 1e-3 -yTo 100 -title "Asia cases" CN KR JP TW TH VN KH SG MN DE')
+os.system('python covid19plot.py -perCapita  -dark -deaths -out Germany_Asia_lin_deaths -yFrom 0 -yTo 1 -title "Asia deaths" CN KR JP TW TH VN KH SG MN DE')
+os.system('python covid19plot.py -perCapita  -dark  -out Germany_Asia_lin_Cases -yFrom 0 -yTo 100 -title "Asia cases" CN KR JP TW TH VN KH SG MN DE')
